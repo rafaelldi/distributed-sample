75d979e08d7bbc0e3051ce0b3cef14a74a975417
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2022-12-04 22:11:34 +0100

    Some changes

---
+++ worker/MyDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace worker;
+
+public class MyDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public DbSet<Order> Orders { get; set; }
+
+    public MyDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseNpgsql(_configuration.GetConnectionString("Postgres"));
+    }
+}
+
+public record Order(Guid Id, string OrderId);
--- worker/Program.cs
+++ worker/Program.cs
@@ -1,23 +1,34 @@
 using contracts;
 using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using worker;
 
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        services.AddDbContext<MyDbContext>();
         services.AddMassTransit(x =>
             {
                 x.SetKebabCaseEndpointNameFormatter();
 
                 x.UsingRabbitMq((context, cfg) => cfg.ConfigureEndpoints(context));
 
-                x.AddHandler(async (SubmitOrderRequest request) =>
+                x.AddHandler(async (SubmitOrderRequest request, MyDbContext context) =>
                 {
-                    await Task.Delay(TimeSpan.FromSeconds(1));
+                    context.Orders.Add(new Order(Guid.NewGuid(), request.Id));
+                    await context.SaveChangesAsync();
+
                     return new SubmitOrderResponse($"Order {request.Id} accepted");
                 });
             }
         );
     })
     .Build();
+
+using (var scope = host.Services.CreateScope())
+{
+    var myContext = scope.ServiceProvider.GetRequiredService<MyDbContext>();
+    myContext.Database.Migrate();
+}
 
 await host.RunAsync();
